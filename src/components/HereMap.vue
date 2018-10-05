468fca190d6c8eafabb9de9c76a6a3380a98c314
Sunimal Malkakulage <SunimalM@synergenhealth.com>
2018-10-05 11:38:50 +0530

    HereMap component

---
+++ src/components/HereMap.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="here-map"></div>
+</template>
+
+<script >
+export default {
+    name : "HereMap",
+    data(){},
+    props:{},
+    created(){},
+    mounted(){}
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
